bf701bad3c3da4fead0596cfba370af1f76a0b1f
PMCANOAS\gustavo.bsantos <gustavo.bsantos@canoastec.rs.gov.br>
2025-02-20 16:55:43 -0300

    cookin up first commit

---
+++ Vue3 - entendendo componentes, diretivas e reatividade no framework/cookin-up/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Banner />
+</template>
+
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import Banner from './components/Banner.vue';
+  export default defineComponent({
+    components: {
+      Banner
+    }
+  })
+</script>
